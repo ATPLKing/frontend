b1b143d92e4456bda19b3bc5083ca11b4ae6ac6e
Chesterkxng <cgoita00@gmail.com>
2025-07-04 19:43:48 +0200

    chore: questions filters utils

---
+++ assets/js/utils/question.js
@@ -0,0 +1,26 @@
+/**
+ * Filters questions based on a given list of subtopics.
+ *
+ * @param {Array} questions - The array of question objects to filter.
+ * @param {Array} subtopics - The list of subtopics to filter by.
+ * @returns {Array} The filtered array of questions.
+ */
+function filterQuestionsBySubtopics(questions, subtopics) {
+  return questions.filter((question) => subtopics.includes(question.subtopic));
+}
+
+/**
+ * Filters questions based on a given database key.
+ *
+ * @param {Array} questions - The array of question objects to filter.
+ * @param {string} databaseKey - The key of database to filter by.
+ * @returns {Array} The filtered array of questions.
+ */
+function filterQuestionsByDatabase(questions, databaseKey) {
+  const databaseFilter = ["", databaseKey]; // blank added to get non restricted questions
+  return questions.filter((question) =>
+    databaseFilter.includes(question.database)
+  );
+}
+
+export { filterQuestionsBySubtopics, filterQuestionsByDatabase };
