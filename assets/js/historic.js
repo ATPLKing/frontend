f1af74db88803fb14c5a368e5112563ec22c338a
Chesterkxng <cgoita00@gmail.com>
2025-08-02 04:49:23 +0200

    chore: historic script js file

---
+++ assets/js/historic.js
@@ -0,0 +1,72 @@
+import { themeHandler } from "./theme.js";
+import { fillHistoricTableRows } from "./render.js";
+
+const AllTests = JSON.parse(localStorage.getItem("tests") || "{}");
+
+function initializeApp() {
+  themeHandler();
+  fillHistoricTableRows(AllTests);
+  setupHistoricTableActions();
+
+  $(document).ready(function () {
+    $("#historic-table").DataTable({
+      responsive: true,
+      language: {
+        url: "//cdn.datatables.net/plug-ins/1.13.6/i18n/fr-FR.json",
+      },
+    });
+  });
+}
+
+document.addEventListener("DOMContentLoaded", initializeApp);
+
+function setupHistoricTableActions() {
+  document
+    .querySelector("#historic-table tbody")
+    .addEventListener("click", function (e) {
+      const tr = e.target.closest("tr");
+      if (!tr) return;
+
+      const testId = tr.getAttribute("data-test-id");
+
+      if (e.target.closest(".result")) {
+        handleShowTestResult(testId);
+      } else if (e.target.closest(".test")) {
+        handleOpenTest(testId);
+      } else if (e.target.closest(".delete")) {
+        handleDeleteTest(testId);
+      }
+    });
+}
+
+function handleShowTestResult(testId) {
+  const test = AllTests[testId];
+  if (test.questions.length != test.userAnswers.length) {
+    alertify.alert(
+      "Test incomplet",
+      "Impossible d'acceder aux résultats, veuillez terminer le test !"
+    );
+    return;
+  }
+
+  localStorage.setItem("current-test-id", testId);
+  window.location.href = "/result";
+}
+
+function handleOpenTest(testId) {
+  localStorage.setItem("current-test-id", testId);
+  window.location.href = "/quiz";
+}
+
+function handleDeleteTest(testId) {
+  alertify
+    .confirm(`Êtes-vous sûr de vouloir supprimer le test ?`)
+    .set("title", "Supprimer le test")
+    .set("labels", { ok: "OUI", cancel: "NON" })
+    .set("onok", function () {
+      delete AllTests[testId];
+      localStorage.setItem("tests", JSON.stringify(AllTests));
+      window.location.href = "/historic";
+    })
+    .set("oncancel", function () {});
+}
