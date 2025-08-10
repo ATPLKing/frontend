f909b6a97a970e1e23f49de7198d9d79add07cd7
Chesterkxng <cgoita00@gmail.com>
2025-08-10 17:16:13 +0200

    chore: retest action implementation and docstrings

--- assets/js/historic.js
+++ assets/js/historic.js
@@ -1,6 +1,12 @@
 import { themeHandler } from "./theme.js";
 import { fillHistoricTableRows } from "./render.js";
-import { loadSavedTests , deleteTest } from "./utils/test.js";
+import {
+  loadSavedTests,
+  createTest,
+  deleteTest,
+  startTest,
+  saveTest,
+} from "./utils/test.js";
 
 const savedTests = loadSavedTests();
 
@@ -36,10 +42,19 @@ function setupHistoricTableActions() {
         handleOpenTest(testId);
       } else if (e.target.closest(".delete")) {
         handleDeleteTest(testId);
+      } else if (e.target.closest(".retest")) {
+        handleRetest(testId);
       }
     });
 }
 
+
+/**
+ * Handles showing the result of a specific test.
+ * It verifies if the test is complete before storing its ID in localStorage
+ * and redirecting the user to the results page.
+ * @param {string} testId - The ID of the test to display.
+ */
 function handleShowTestResult(testId) {
   const test = savedTests[testId];
   if (test.questions.length != test.userAnswers.length) {
@@ -54,11 +69,24 @@ function handleShowTestResult(testId) {
   window.location.href = "/result";
 }
 
+
+/**
+ * Sets the current test ID in local storage and redirects to the quiz page.
+ * @param {string | number} testId The ID of the test to be opened.
+ */
 function handleOpenTest(testId) {
   localStorage.setItem("current-test-id", testId);
   window.location.href = "/quiz";
 }
 
+
+/**
+ * Handles the deletion of a test after user confirmation.
+ * Displays a confirmation dialog using alertify. If confirmed,
+ * it deletes the test, shows a success notification, and redirects
+ * to the historic page.
+ * @param {string} testId - The ID of the test to be deleted.
+ */
 function handleDeleteTest(testId) {
   alertify
     .confirm(`Êtes-vous sûr de vouloir supprimer le test ?`)
@@ -66,12 +94,33 @@ function handleDeleteTest(testId) {
     .set("labels", { ok: "OUI", cancel: "NON" })
     .set("onok", function () {
       const test = savedTests[testId];
-      deleteTest(test)
+      deleteTest(test);
       alertify.success("Test supprimé");
 
       setTimeout(() => {
         window.location.href = "/historic";
       }, 2000);
+    })
+    .set("oncancel", function () {});
+}
+
+
+/**
+ * Prompts the user to confirm restarting a test.
+ * If confirmed, creates a new test using the parameters of the old test,
+ * saves it, and starts the quiz.
+ * @param {string} testId - The ID of the test to be retaken.
+ */
+function handleRetest(testId) {
+  alertify
+    .confirm(`Êtes-vous sûr de vouloir recommencer ce test ?`)
+    .set("title", "Recommencer le test")
+    .set("labels", { ok: "OUI", cancel: "NON" })
+    .set("onok", function () {
+      const oldTest = savedTests[testId];
+      const newTest = createTest(oldTest.params);
+      saveTest(newTest);
+      startTest(newTest);
     })
     .set("oncancel", function () {});
 }
